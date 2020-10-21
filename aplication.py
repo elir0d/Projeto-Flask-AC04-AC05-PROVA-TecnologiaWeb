257001525ba70aed2112b16604e29fbeb3f03ede
Rodrigo Trindade <roelisontrindade@outlook.com>
2020-10-21 00:24:32 -0300

    Defined boiler plate

--- aplication.py
+++ aplication.py
@@ -8,4 +8,12 @@ app = Flask (__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/details')
+def details():
+    return render_template("details.html")
+
+# if __name__ == "__main__":
+#     port = int( os.environ.get("PORT", 5000) )
+#     app.run( host='0.0.0.0', port = port )
+
 app.run()
