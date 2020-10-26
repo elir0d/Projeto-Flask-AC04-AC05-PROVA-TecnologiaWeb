7d8a8db55cba99acc4e05e3b2fe172e056bccdb1
Rodrigo Macade <49011043+rodrigomacade@users.noreply.github.com>
2020-10-26 09:45:48 -0300

    Update application.py

--- application.py
+++ application.py
@@ -26,6 +26,3 @@ def circuitos():
 if __name__ == "__main__":
     port = int( os.environ.get("PORT", 5000) )
     app.run( host='0.0.0.0', port = port )
-
-# if __name__ == '__main__':
-#     app.run('localhost', 5555)
