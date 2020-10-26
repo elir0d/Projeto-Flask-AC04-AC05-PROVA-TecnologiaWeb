257bd94a1696a288c4777d5d8775350732cbe976
Rodrigo Trindade <roelisontrindade@outlook.com>
2020-10-26 01:00:28 -0300

    Fixed routes

--- application.py
+++ application.py
@@ -11,7 +11,18 @@ def index():
 @app.route('/competidores')
 def competidores():
     return render_template("competidores.html")
+@app.route('/campeonato')
+def campeonato():
+    return render_template("campeonato.html")
 
+@app.route('/vitorias')
+def vitorias():
+    return render_template("vitorias.html")
+
+@app.route('/circuitos')
+def circuitos():
+    return render_template("circuitos.html")
+    
 # if __name__ == "__main__":
 #     port = int( os.environ.get("PORT", 5000) )
 #     app.run( host='0.0.0.0', port = port )
