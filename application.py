e8e0c016dc6b8a1d1bac7838ed1c400a8986d051
Rodrigo Trindade <roelisontrindade@outlook.com>
2020-10-26 01:40:22 -0300

    ready to deploy

--- application.py
+++ application.py
@@ -23,9 +23,9 @@ def vitorias():
 def circuitos():
     return render_template("circuitos.html")
     
-# if __name__ == "__main__":
-#     port = int( os.environ.get("PORT", 5000) )
-#     app.run( host='0.0.0.0', port = port )
+if __name__ == "__main__":
+    port = int( os.environ.get("PORT", 5000) )
+    app.run( host='0.0.0.0', port = port )
 
-if __name__ == '__main__':
-    app.run('localhost', 5555)
+# if __name__ == '__main__':
+#     app.run('localhost', 5555)
