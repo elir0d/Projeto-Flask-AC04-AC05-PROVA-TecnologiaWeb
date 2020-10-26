e94159a3728ee82d035dc505a9c02a1f052e95f4
Rodrigo Trindade <roelisontrindade@outlook.com>
2020-10-26 10:05:55 -0300

    App ready to staging and production

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------#
 import os
 from flask import Flask
 from flask import render_template
@@ -27,5 +28,12 @@ if __name__ == "__main__":
     port = int( os.environ.get("PORT", 5000) )
     app.run( host='0.0.0.0', port = port )
 
+
+
+
+
+# ----------------Development run------------------------#
 # if __name__ == '__main__':
 #     app.run('localhost', 5555)
+# -------------------------------------------------------#
+
