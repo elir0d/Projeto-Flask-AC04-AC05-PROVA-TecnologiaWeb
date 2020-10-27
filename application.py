a7d44ccbed3e3b047da6d811009e24c8dfd07577
Rodrigo Trindade <roelisontrindade@outlook.com>
2020-10-27 10:11:11 -0300

    Fixed index route app

--- application.py
+++ application.py
@@ -5,8 +5,9 @@ from flask import render_template
 app = Flask (__name__)
     
 @app.route('/')
-def home():
-    return render_template("home.html")
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
 @app.route('/competidores')
 def competidores():
